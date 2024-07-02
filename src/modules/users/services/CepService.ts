28859baac59a4fe9797d05770591c5fc4b722523
Helison Pontes <helissonpontes69@gmail.com>
2024-07-02 12:40:46 -0300

    feat: created cep service with external api

---
+++ src/modules/users/services/CepService.ts
@@ -0,0 +1,23 @@
+import { CEP_INVALID } from '@shared/consts/ErrorMessagesConsts';
+import { BusinessError } from '@shared/errors/BusinessError';
+import axios from 'axios';
+import { IAddressData } from '../domain/models/IAdressData';
+
+export async function CepService(cep: string) {
+  const response = await axios.get(`https://viacep.com.br/ws/${cep}/json`);
+
+  if (response.data.erro) {
+    throw new BusinessError(CEP_INVALID);
+  }
+
+  const addressData: IAddressData = {
+    complement:
+      response.data.complemento !== '' ? response.data.complemento : 'N/A',
+    neighborhood: response.data.bairro !== '' ? response.data.bairro : 'N/A',
+    locality:
+      response.data.localidade !== '' ? response.data.localidade : 'N/A',
+    uf: response.data.uf !== '' ? response.data.uf : 'N/A',
+  };
+
+  return addressData;
+}
