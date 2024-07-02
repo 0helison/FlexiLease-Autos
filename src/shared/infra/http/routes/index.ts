478a05d14c13308cabf5a2c2b344292ef7560f1d
Helison Pontes <helissonpontes69@gmail.com>
2024-07-02 13:37:40 -0300

    feat: added all routes of application

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,9 +1,14 @@
+import carsRouter from '@modules/cars/infra/http/routes/car.routes';
+import reserveRouter from '@modules/reserve/infra/http/routes/reserve.routes';
+import authRouter from '@modules/users/infra/http/routes/auth.routes';
+import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (request, response) => {
-  return response.json({ message: 'Hello World!' });
-});
+routes.use('/api/v1/users', usersRouter);
+routes.use('/api/v1/auth', authRouter);
+routes.use('/api/v1/cars', carsRouter);
+routes.use('/api/v1/reserve', reserveRouter);
 
 export default routes;
