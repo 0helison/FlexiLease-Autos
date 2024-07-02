7eb0b80783ed68b717027c2ccce8d2e35d915c31
Helison Pontes <helissonpontes69@gmail.com>
2024-07-02 12:20:10 -0300

    feat: created dependency injection for repositories

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,1 +1,27 @@
+import { IUsersRepository } from '@modules/users/domain/repositories/IUserRepository';
+import { IUserTokensRepository } from '@modules/users/domain/repositories/IUserTokenRepository';
+import UsersRepository from '@modules/users/infra/typeorm/repositories/UserRepository';
+import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokenRepository';
 import { container } from 'tsyringe';
+import '@modules/users/providers';
+import { ICarRepository } from '@modules/cars/domain/repositories/ICarRepository';
+import CarRepository from '@modules/cars/infra/typeorm/repositories/CarRepository';
+import { IReserveRepository } from '@modules/reserve/domain/repositories/IReserveRepository';
+import ReserveRepository from '@modules/reserve/infra/typeorm/repositories/ReserveRepository';
+
+container.registerSingleton<IUsersRepository>(
+  'UsersRepository',
+  UsersRepository,
+);
+
+container.registerSingleton<IUserTokensRepository>(
+  'UserTokensRepository',
+  UserTokensRepository,
+);
+
+container.registerSingleton<ICarRepository>('CarRepository', CarRepository);
+
+container.registerSingleton<IReserveRepository>(
+  'ReserveRepository',
+  ReserveRepository,
+);
