daf96b2ca6de1e5c6093d819cc3af8a461d616ff
Helison Pontes <helissonpontes69@gmail.com>
2024-07-02 13:32:34 -0300

    feat: added date formatting for reserve

---
+++ src/modules/reserve/utils/dateUtils.ts
@@ -0,0 +1,39 @@
+import { isAfter, parse } from 'date-fns';
+import { IReserve } from '../domain/models/IReserve';
+import { formatDate } from '@shared/format/FormatDate';
+
+export function generateDateRange(start: Date, end: Date): Date[] {
+  const dateArray: Date[] = [];
+  const currentDate = new Date(start);
+
+  while (currentDate <= end) {
+    dateArray.push(new Date(currentDate));
+    currentDate.setDate(currentDate.getDate() + 1);
+  }
+
+  return dateArray;
+}
+
+export function parseDateParams(
+  dateString: string,
+  formatString: string = 'dd/MM/yyyy',
+): Date | undefined {
+  const date = parse(dateString, formatString, new Date());
+  return isNaN(date.getTime()) ? undefined : date;
+}
+
+export function formatReserve(reserve: IReserve) {
+  return {
+    ...reserve,
+    start_date: formatDate(reserve.start_date),
+    end_date: formatDate(reserve.end_date),
+  };
+}
+
+export function formatReserves(reserves: IReserve[]) {
+  return reserves.map(reserve => formatReserve(reserve));
+}
+
+export function dateOrderValidation(startDate: Date, endDate: Date): boolean {
+  return isAfter(startDate, endDate);
+}
