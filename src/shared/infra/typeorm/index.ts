c81fd5d855c74f26497be2c1ce42507a93b60377
Helison Pontes <helissonpontes69@gmail.com>
2024-07-02 13:38:10 -0300

    feat: added config of datasource

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,11 +1,14 @@
 import { DataSource } from 'typeorm';
+import { User } from '@modules/users/infra/typeorm/entities/User';
+import { UserToken } from '@modules/users/infra/typeorm/entities/UserToken';
+import { Car } from '@modules/cars/infra/typeorm/entities/Cars';
+import { Reserve } from '@modules/reserve/infra/typeorm/entities/Reserve';
 
-const dataSource = new DataSource({
-  type: 'sqlite',
-  database: 'src/database/compacine.sqlite',
-  entities: [],
-  migrations: [],
+export const dataSource = new DataSource({
+  type: 'mongodb',
+  database: 'api',
   synchronize: true,
+  host: 'localhost',
+  port: 27017,
+  entities: [UserToken, User, Car, Reserve],
 });
-
-export { dataSource };
