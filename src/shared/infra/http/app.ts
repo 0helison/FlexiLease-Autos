7bc942f10c81ed7162272f6869c42d18772c6297
Helison Pontes <helissonpontes69@gmail.com>
2024-07-02 12:54:15 -0300

    chore: developmental spells

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,10 +1,11 @@
-import express, { Request, Response, NextFunction } from 'express';
+import 'reflect-metadata';
+import express from 'express';
 import 'express-async-errors';
 import cors from 'cors';
 import routes from './routes';
-import AppError from '@shared/errors/AppError';
 import '@shared/infra/typeorm';
 import '@shared/container';
+import { errorHandlerMiddleware } from './middlewares/errorHandlerMiddleware';
 
 const app = express();
 
@@ -13,22 +14,6 @@ app.use(express.json());
 
 app.use(routes);
 
-app.use(
-  (error: Error, request: Request, response: Response, next: NextFunction) => {
-    if (error instanceof AppError) {
-      return response.status(error.statusCode).json({
-        status: 'error',
-        message: error.message,
-      });
-    }
-
-    console.log(error);
-
-    return response.status(500).json({
-      status: 'error',
-      message: 'Internal server error',
-    });
-  },
-);
+app.use(errorHandlerMiddleware);
 
 export { app };
