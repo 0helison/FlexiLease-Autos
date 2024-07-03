bc02fe90e3025a154289dabc30f29393183410e1
Helison Pontes <helissonpontes69@gmail.com>
2024-07-03 14:13:26 -0300

    chore: added tests config

---
+++ vite.config.mjs
@@ -0,0 +1,30 @@
+import dotenv from 'dotenv';
+import swc from 'unplugin-swc';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
+
+dotenv.config({ path: '.env.test' });
+
+export const userConfig = {
+  plugins: [
+    tsconfigPaths(),
+    swc.vite({
+      module: { type: 'es6' },
+    }),
+  ],
+  test: {
+    globals: true,
+    root: './',
+    coverage: {
+      exclude: [
+        '**/*index.ts',
+        '**/*server.ts',
+        '**/*Table.ts',
+        '**/*.config.mjs',
+      ],
+    },
+    testTimeout: 10000,
+  },
+};
+
+export default defineConfig(userConfig);
