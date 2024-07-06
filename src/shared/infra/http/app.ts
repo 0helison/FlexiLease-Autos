81339a1e2d7d4dc64936ea2de8f061668ea75229
Helison Pontes <helissonpontes69@gmail.com>
2024-07-06 11:14:41 -0300

    feat: added swagger

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -3,6 +3,8 @@ import express from 'express';
 import 'express-async-errors';
 import cors from 'cors';
 import routes from './routes';
+import swaggerUi from 'swagger-ui-express';
+import swaggerFile from './swagger_output.json';
 import '@shared/infra/typeorm';
 import '@shared/container';
 import { errorHandlerMiddleware } from './middlewares/errorHandlerMiddleware';
@@ -13,6 +15,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+app.use('/api/v1/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(errorHandlerMiddleware);
 
