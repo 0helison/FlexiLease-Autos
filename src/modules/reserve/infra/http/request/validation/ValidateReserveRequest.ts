e1e20b0dc115dfe6657f3144692a642d4881f6e0
Helison Pontes <helissonpontes69@gmail.com>
2024-07-07 19:24:26 -0300

    refactor: swagger and zod reserve schema

--- src/modules/reserve/infra/http/request/validation/ValidateReserveRequest.ts
+++ src/modules/reserve/infra/http/request/validation/ValidateReserveRequest.ts
@@ -1,15 +1,26 @@
-import { customDateValidation } from '@shared/format/FormatDate';
+import { ObjectId } from 'mongodb';
 import { minLength } from '@shared/format/Minimum';
 import { z } from 'zod';
+import { customDateValidation } from '@shared/format/FormatDate';
+
+function isValidObjectId(value: string): boolean {
+  return ObjectId.isValid(value);
+}
 
 export const ZodReserveSchema = z.object({
   _id_user: z
     .string()
-    .refine(minLength(1), { message: "The '_id_user' field is mandatory." }),
+    .refine(minLength(1), { message: "The '_id_user' field is mandatory." })
+    .refine(value => isValidObjectId(value), {
+      message: "The '_id_user' field must be a valid ObjectId.",
+    }),
 
   _id_car: z
     .string()
-    .refine(minLength(1), { message: "The '_id_car' field is mandatory." }),
+    .refine(minLength(1), { message: "The '_id_car' field is mandatory." })
+    .refine(value => isValidObjectId(value), {
+      message: "The '_id_car' field must be a valid ObjectId.",
+    }),
 
   start_date: z
     .string()
